95e3d9196e5d1310e0cc32f3a30ec7b8e511b613
Tilman Stehr <tilman@tilman.ninja>
2018-02-18 23:59:06 +0100

    Set start_url in manifest to /
    Closes #10

--- client/assets/rebuild_icons.sh
+++ client/assets/rebuild_icons.sh
@@ -1,5 +1,11 @@
 #! /usr/bin/env sh
 
+
 convert -density 384 -background transparent shopping-basket.svg  -define icon:auto-resize ../public/favicon.ico
-convert -border 100 -bordercolor white shopping-basket.svg ../public/apple-touch-icon.png
+
+let "border = 32 * 3"
+width=512
+let "actual_width = $width - 2 * $border"
+inkscape -z -e $(pwd)/TMP_apple-touch-icon.png -w $actual_width -h $actual_width $(pwd)/shopping-basket.svg
+convert -border 75 -bordercolor white TMP_apple-touch-icon.png  ../public/apple-touch-icon.png
 cp shopping-basket.svg ../public/mask-icon.svg
