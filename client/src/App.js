ba773ea668af4bc6ee55d4a53cfd9f2a85032933
Tilman Stehr <tilman@tilman.ninja>
2018-03-23 23:15:58 +0100

    Move router component to app, remove unused offline tracking

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,29 @@
 // @flow
 import React, { Component } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import ChooseListComponent from './ChooseListComponent'
+import ShoppingListComponent from './ShoppingListContainerComponent'
 import ShoppingListContainerComponent from './ShoppingListContainerComponent'
 
-type State = {
-  offline: boolean
-}
 
 function ShoppingListContainerWrapperComponent(props) {
   return <ShoppingListContainerComponent listid={props.match.params.listid} />
 }
 
-export default class App extends Component<void, State> {
-  constructor(props: void) {
-    super(props)
-
-    this.state = {
-      offline: !window.navigator.onLine
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener('offline', this.handleOffline)
-    window.addEventListener('online', this.handleOnline)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('offline', this.handleOffline)
-    window.removeEventListener('online', this.handleOnline)
-  }
-
-  handleOffline = () => {
-    this.setState({
-      offline: true
-    })
-  }
-
-  handleOnline = () => {
-    this.setState({
-      offline: false
-    })
-  }
-
-
+export default class App extends Component<void> {
   render() {
     return (
-      <Switch>
-        <Route exact path='/' component={ChooseListComponent}/>
-        <Route path='/:listid' exact component={ShoppingListContainerWrapperComponent}/>
-        <Route path='/:listid/orders' exact component={ShoppingListContainerWrapperComponent}/>
-        <Route path='/:listid/orders/:orderid' exact component={ShoppingListContainerWrapperComponent}/>
-        <Route path='/:listid/:itemid/category' exact component={ShoppingListContainerWrapperComponent}/>
-        <Route component={Error404}/>
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={ChooseListComponent}/>
+          <Route path='/:listid' exact component={ShoppingListContainerWrapperComponent}/>
+          <Route path='/:listid/orders' exact component={ShoppingListContainerWrapperComponent}/>
+          <Route path='/:listid/orders/:orderid' exact component={ShoppingListContainerWrapperComponent}/>
+          <Route path='/:listid/:itemid/category' exact component={ShoppingListContainerWrapperComponent}/>
+          <Route component={Error404}/>
+        </Switch>
+      </BrowserRouter>
     )
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
-  <BrowserRouter><App/></BrowserRouter>
+  <App/>
 ), document.getElementById('root'));
 registerServiceWorker();
