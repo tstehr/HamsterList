f7887973684cfaf06b742faeb4621a9334679491
Tilman Stehr <tilman@tilman.ninja>
2018-06-05 18:59:47 +0200

    Fix test for two-way merge

--- shared/src/data/ShoppingList.test.js
+++ shared/src/data/ShoppingList.test.js
@@ -259,7 +259,8 @@ describe('mergeShoppingLists', () => {
           name: 'Dosen Kichererbsen',
           category: 'bef7bffc-6f54-450a-804e-799d1da5b976',
           amount: {
-            value: 2
+            value: 2,
+            unit: undefined,
           },
           id: 'c14ef9de-3075-445e-9225-6a50e0c0adca'
         },
@@ -282,6 +283,12 @@ describe('mergeShoppingLists', () => {
           id: '69fab191-4a93-41a5-a7c0-2a1b2ae2dcbd'
         },
         {
+          name: "Knoblauch",
+          category: "6ca0f054-209c-46c9-b337-6088f7a530ab",
+          amount: undefined,
+          id: "5d0d6c63-67d4-4f3f-8c00-540823e0476c"
+        },
+        {
           name: 'Käse',
           category: '7ca893d0-bc4f-4a79-bdc1-3853cec70001',
           amount: undefined,
@@ -292,7 +299,19 @@ describe('mergeShoppingLists', () => {
           category: '1508d447-3e0d-4b50-bcae-ae4a9c85a3ea',
           amount: undefined,
           id: 'cbda3946-f136-4c94-8280-4931100576b4'
-        }
+        },
+        {
+          name: "MiniMilk",
+          category: "b68d25e8-5fa0-4f83-9408-6c306ddb8c21",
+          amount: undefined,
+          id: "1ccf57f6-5452-4de4-8e04-b908a4144158",
+        },
+        {
+          name: "Petersilie",
+          category: "6ca0f054-209c-46c9-b337-6088f7a530ab",
+          amount: undefined,
+          id: "5b8f0e0e-6663-4f0a-abe8-dbb7f9784579",
+        },
       ]
     })
   })
