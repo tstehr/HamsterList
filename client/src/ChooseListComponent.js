tstehr/HamsterList

commit c729306b90a3c25ed128bf6c212f636a9473c0d8

Fix choosing list by name in safari

client/src/ChooseListComponent.js
@@ -19,12 +19,11 @@ export default class ChooseListComponent extends Component<void, State> {
   }
 
   onSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
-    const formData = new FormData(e.currentTarget)
+    e.preventDefault()
     this.setState({
       // $FlowFixMe
-      listid: formData.get('listid').trim()
+      listid: e.currentTarget.elements['listid'].value
     })
-    e.preventDefault()
   }
 
   async createRandomList() {

commit 6a51a4513bc768b9608ef28b35d2f2fc044a4c01

Fix regression, not trimming listid in list chooser
Was caused by c729306b90a3c25ed128bf6c212f636a9473c0d8

client/src/ChooseListComponent.js
@@ -22,7 +22,7 @@ export default class ChooseListComponent extends Component<void, State> {
     e.preventDefault()
     this.setState({
       // $FlowFixMe
-      listid: e.currentTarget.elements['listid'].value
+      listid: e.currentTarget.elements['listid'].value.trim()
     })
   }
 