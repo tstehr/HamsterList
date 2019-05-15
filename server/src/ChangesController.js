534eaf87e206062b67a53fb18dccfb30e52e87a3
Tilman Stehr <tilman@tilman.ninja>
2019-05-15 23:11:49 +0200

    Implement limits for changes endpoint

--- server/src/ChangesController.js
+++ server/src/ChangesController.js
@@ -5,7 +5,11 @@ import { type ShoppingListRequest } from './ShoppingListController'
 
 export default class ChangesController {
   handleGet = (req: ShoppingListRequest, res: express$Response, next: express$NextFunction) => {
-    res.json(req.list.changes)
+    const oldestIndex = _.findIndex(req.list.changes, (c) => c.token === req.query['oldest'])
+    const startIndex = oldestIndex === -1 ? 0 : oldestIndex
+    const newestIndex =  _.findIndex(req.list.changes, (c) => c.token === req.query['newest'])
+    const endIndex = newestIndex === -1 ? req.list.changes.length : newestIndex + 1
+    res.json(req.list.changes.slice(startIndex, endIndex))
     next()
   }
 }
