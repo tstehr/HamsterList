189e25f578f3186df4ad27fd6bb14d30eded76ee
Tilman Stehr <tilman@tilman.ninja>
2020-01-09 00:09:04 +0100

    Hide unnecessary stuff for printing

--- client/src/EditItemComponent.js
+++ client/src/EditItemComponent.js
@@ -147,7 +147,7 @@ export default class EditItemComponent extends Component<Props, State> {
                 <ItemComponent item={this.props.item} />
               </div>
           }
-          <IconButton onClick={(e) => this.props.deleteItem(this.props.item.id)} icon="DELETE" alt="Delete" className="KeyFocusComponent--noFocus"/>
+          <IconButton onClick={(e) => this.props.deleteItem(this.props.item.id)} icon="DELETE" alt="Delete" className="EditItemComponent__delete KeyFocusComponent--noFocus"/>
         </li>
     )
   }
