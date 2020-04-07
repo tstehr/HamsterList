0f533cbbb32ddc365745ba78eca80b6e4561648c
Tilman Stehr <tilman@tilman.ninja>
2020-04-07 19:07:25 +0200

    Use yarn workspace install in install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,4 @@
 #! /usr/bin/env sh
 
-# all installs are run in parallel
-
-cd shared
-yarn install | sed "s/^/[shared] /" &
-shared_pid=$!
-
-cd ../server
-yarn install | sed "s/^/[client] /" &
-server_pid=$!
-
-cd ../client
-yarn install | sed "s/^/[server] /" &
-client_pid=$!
-
-wait $shared_pid
-wait $server_pid
-wait $client_pid
+# install using yarn workspace
+yarn install
