c0a1222682aca0a4c0481dc3c61667fb494d3cf6
Tilman Stehr <tilman@tilman.ninja>
2020-04-08 18:26:44 +0200

    Add type-dependent eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['*/tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier/@typescript-eslint',
+  ],
+  rules: {
+    '@typescript-eslint/no-use-before-define': ['error', 'nofunc'],
+    '@typescript-eslint/explicit-function-return-type': ['warn', { allowExpressions: true }],
+    '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/ban-types': ['error'],
+    '@typescript-eslint/no-extra-non-null-assertion': ['error'],
+    '@typescript-eslint/no-implied-eval': ['error'],
+    //'@typescript-eslint/no-floating-promises': ['error'],
+    '@typescript-eslint/no-throw-literal': ['error'],
+    // '@typescript-eslint/no-unnecessary-condition': ['warn'],
+    '@typescript-eslint/no-unnecessary-qualifier': ['warn'],
+    '@typescript-eslint/no-unnecessary-type-arguments': ['warn'],
+    '@typescript-eslint/prefer-nullish-coalescing': ['warn'],
+    '@typescript-eslint/prefer-optional-chain': ['warn'],
+    '@typescript-eslint/restrict-plus-operands': ['error'],
+  },
+  overrides: [
+    {
+      files: ['*.test.ts'],
+      rules: {
+        '@typescript-eslint/ban-ts-ignore': 'off',
+      },
+    },
+  ],
+}
