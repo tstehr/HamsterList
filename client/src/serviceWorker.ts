30f39a47b9d55b76af070e0185a0845ad53bb8dc
Tilman Stehr <tilman@tilman.ninja>
2020-05-07 23:54:52 +0200

    Fix service worker registration

--- client/src/serviceWorker.ts
+++ client/src/serviceWorker.ts
@@ -35,7 +35,7 @@ export function register(config?: Config): void {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker-custom.js`
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
