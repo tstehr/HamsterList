d70f028d21fe4af5afb4bcc1061cbbfa2c89226d
Tilman Stehr <tilman@tilman.ninja>
2024-03-30 23:26:13 +0100

    Add script for normalization of existing listids

---
+++ server/scripts/normalizeListsWherePossible.ts
@@ -0,0 +1,47 @@
+import fs from 'fs-extra'
+import path from 'path'
+import _ from 'lodash'
+import normalizeListid from '../src/util/normalizeListid'
+
+const PATH = path.resolve(process.argv[2])
+console.log(`Reading ${PATH}`)
+
+const db = fs.readJsonSync(PATH)
+const normalizedToId = new Map<string, string[]>()
+
+for (const list of db.lists) {
+    const normalizedId = normalizeListid(list.id)
+    
+    let unnormalizedIds = normalizedToId.get(normalizedId)
+    if (!unnormalizedIds) {
+        unnormalizedIds = [list.id]
+        normalizedToId.set(normalizedId, unnormalizedIds)
+    } else {
+        unnormalizedIds.push(list.id)
+    }
+}
+
+const idToNormalized = new Map<string, string>()
+
+for (const [normalizedId, unnormalizedIds] of normalizedToId.entries()) {
+    if (unnormalizedIds.length > 1) {
+        console.log('Skipped', [normalizedId, unnormalizedIds])
+        continue
+    }
+    const [unnormalizedId] = unnormalizedIds
+    if (normalizedId === unnormalizedId) {
+        continue
+    }
+    idToNormalized.set(unnormalizedId, normalizedId)
+}
+
+for (const list of db.lists) {
+    const normalizedId = idToNormalized.get(list.id)
+    if (normalizedId) {
+        list.id = normalizedId
+    }
+}
+
+fs.outputJSON(PATH, db, { spaces: 2 })
+
+console.log('Done')
