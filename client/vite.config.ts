5e61cdc6ebe941eb8b38b355aa72d2ed5dc35751
Tilman Stehr <tilman@tilman.ninja>
2024-04-24 23:57:31 +0200

    Basic vite setup for client

---
+++ client/vite.config.ts
@@ -0,0 +1,22 @@
+import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer'
+import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), viteTsconfigPaths({ root: '.' }), svgr()],
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
+  build: {
+    outDir: 'build',
+    sourcemap: true,
+  },
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
+  },
+})
