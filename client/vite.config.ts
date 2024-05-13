960b2eeb2fdf281ddfa45af7435eca0b8a386c6a
Tilman Stehr <tilman@tilman.ninja>
2024-05-14 00:11:52 +0200

    Rework git hash injection for yarn berry

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,43 +1,52 @@
 import react from '@vitejs/plugin-react'
 import autoprefixer from 'autoprefixer'
+import { execSync } from 'child_process'
 import { defineConfig } from 'vite'
 import checker from 'vite-plugin-checker'
 import svgr from 'vite-plugin-svgr'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    viteTsconfigPaths({ root: '.' }),
-    svgr(),
-    checker({
-      typescript: true,
-    }),
-  ],
-  css: {
-    postcss: {
-      plugins: [autoprefixer()],
-    },
-  },
-  build: {
-    outDir: 'build',
-    sourcemap: true,
-  },
-  server: {
-    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000/',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
+export default defineConfig(() => {
+  process.env.VITE_GIT_SHA = execSync('git rev-parse --short HEAD').toString().trim() || undefined
+
+  return {
+    plugins: [
+      react(),
+      viteTsconfigPaths({ root: '.' }),
+      svgr(),
+      checker({
+        typescript: true,
+      }),
+    ],
+
+    css: {
+      postcss: {
+        plugins: [autoprefixer()],
       },
     },
-  },
-  test: {
-    exclude: ['build', 'node_modules'],
-    testTimeout: 20000,
-    environment: 'jsdom',
-  },
+
+    build: {
+      outDir: 'build',
+      sourcemap: true,
+    },
+
+    server: {
+      port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:4000/',
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
+      },
+    },
+
+    test: {
+      exclude: ['build', 'node_modules'],
+      testTimeout: 20000,
+      environment: 'jsdom',
+    },
+  }
 })
