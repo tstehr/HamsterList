98b2cb66d15a7a4aa9afae419be424b3a0a5f068
Tilman Stehr <tilman@tilman.ninja>
2024-05-18 22:55:30 +0200

    Use vite-plugin-checker to lint client during build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig(() => {
       svgr(),
       checker({
         typescript: true,
+        eslint: { useFlatConfig: true, lintCommand: 'eslint .' },
       }),
     ],
 
